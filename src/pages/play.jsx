e6da7406a9b848ec1158ae724394878eb353bf82
Calvin Frank <CalvinAndersonFrank@gmail.com>
2023-02-20 11:49:20 -0500

    testing if reloading fix works on main

--- src/pages/play.jsx
+++ src/pages/play.jsx
@@ -6,19 +6,21 @@ export default function Play() {
   const router = useRouter();
   const size = router.query.size;
   const mode = router.query.mode
-  console.log(size)
+
   const cellsStyles = {
     width:`calc(100% * (1/${size}) - 20px)`,
     height:`calc(100% * (1/${size}) - 20px)`,
   }
   useEffect(() => {
+    console.log("In the useEffect(): size: " + size);
     if(size != 4 && size !=5 && size !=6) {
       router.push({pathname:"/modeChoice"})
     }
     if (mode != "easy" && mode != "normal" && mode != "hard") {
       router.push({pathname:"/modeChoice"})
     }
-  })
+  }, [size]);
+
   const cells = []
   for(let i = 0; i<size*size; i++) {
     cells.push(<div className={styles.cells} style={cellsStyles} key={i}></div>)
@@ -30,4 +32,11 @@ export default function Play() {
       </div>
     </div>
   )
+}
+
+//this fuction keeps the useEffect() from redirecting the page when reloaded
+export async function getServerSideProps(context) {
+  return {
+    props: {}, 
+  }
 }
